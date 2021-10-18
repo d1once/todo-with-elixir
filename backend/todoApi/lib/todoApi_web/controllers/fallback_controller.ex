58aa7448f0bc98593b9d663dd1cf649c4cb428ba
Dionis <dionisenja@gmail.com>
2021-10-18 12:08:54 +0200

    backend done

---
+++ backend/todoApi/lib/todoApi_web/controllers/fallback_controller.ex
@@ -0,0 +1,17 @@
+defmodule TodoApiWeb.FallbackController do
+  use TodoApiWeb, :controller
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(TodoApiWeb.ChangesetView)
+    |> render("error.json", changeset: changeset)
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> put_view(TodoApiWeb.ErrorView)
+    |> render(:"404")
+  end
+end
